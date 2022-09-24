648116c0b881a942c095271f6ff38de79b33b78e
Q-100 <opea5954@gmail.com>
2022-09-24 12:02:01 +0900

    dasd

---
+++ src/data.js
@@ -0,0 +1,36 @@
+export default [
+  {
+    id: 0,
+    content: "02-6137-9605",
+    src: "img/Frame 33.png",
+    button: "tel:02-6137-9605",
+  },
+
+  {
+    id: 1,
+    content: "+82 10-6548-1988",
+    src: "img/Frame 35.png",
+    button: "tel:010-6548-1988",
+  },
+
+  {
+    id: 2,
+    content: "matt@nvrkr.com",
+    src: "img/Frame 30.png",
+    button: "mailto:matt@nvrkr.com",
+  },
+
+  {
+    id: 3,
+    content: "07326 서울시 영등포구 국제금융로 10, Three IFC 45층",
+    src: "img/Frame 31.png",
+    button: "#ContactID",
+  },
+
+  {
+    id: 4,
+    content: "https://www.nvrkr.com/",
+    src: "img/Frame 34.png",
+    button: "https://www.nvrkr.com/",
+  },
+];
